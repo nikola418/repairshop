cb80bbb35c109ccef49510d437794a257820ec12
Nikola Milovanovic <nikolam998@gmail.com>
2025-02-23 15:36:10 +0100

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,29 @@ export default function Home() {
   return (
     <div className="bg-black bg-home-img bg-cover bg-center">
       <main className="flex flex-col justify-center text-center max-w-5xl mx-auto min-h-dvh">
-        <section className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl ">
+        <section className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-lg mx-auto text-white sm:text-2xl ">
           <h1 className="text-4xl font-bold">
             Nikola&apos;s Computer
             <br />
             Repair Shop
           </h1>
-          <address>
-            555 Gateway Lane
-            <br />
-            Kansas City, KS 55555
-          </address>
+          <address>Kragujevac, 34000, Serbia</address>
           <p>Open Daily: 9am to 5pm</p>
-          <Link href="tel:+381629756150" className="hover:underline">
-            +381629756150
-          </Link>
+          <p>
+            Phone:&nbsp;
+            <Link href="tel:+381629756150" className="hover:underline">
+              +381629756150
+            </Link>
+          </p>
+          <p>
+            Email:&nbsp;
+            <Link
+              href="mailto:nikolam998@gmail.com"
+              className="hover:underline"
+            >
+              nikolam998@gmail.com
+            </Link>
+          </p>
         </section>
         <div className="fixed top-5 left-5">
           <NavButton href="/tickets" icon={HomeIcon} label="Home" />
